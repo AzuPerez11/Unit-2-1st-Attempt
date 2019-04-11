409d89200ce43594bf1ef9680f2b34536f36b3d7
AzuPerez11 <az_perez11@hotmail.com>
2019-04-10 23:21:40 -0700

    Final Copy
    Final issue was solved and comments were added. Yeehaw!

--- js/script.js
+++ js/script.js
@@ -2,42 +2,19 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
+//These consts connect to the student list and student item classes through the DOM
 
 const studentList = document.querySelector(".student-list");
 let shorterList = studentList.querySelectorAll("li.student-item");
+
+//This block calls the input and div elements added to the html file specific to adding a search box and button.
 const div = document.createElement('div');
 const pageHeader = document.querySelector('.page-header');
 div.className ="student-search";
 pageHeader.appendChild(div);
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
+// The showPage function shows only 10 items from the length of the student list while hiding the rest.
 
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
 const showPage = (list, page) => {
    const firstItem = (page * 10) - 10;
    const lastItem = (page * 10) - 1; 
@@ -49,12 +26,10 @@ const showPage = (list, page) => {
        }
       }
    }   
-
+// The showPage function is called.
    showPage(shorterList, 1);
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
+
+// The appendPageLinks function divides the length of the items into groups of 10. 
 const appendPageLinks = (list) => {
    let pageNumbers = Math.ceil(list.length / 10);
    const page = document.querySelector('.page');
@@ -63,6 +38,7 @@ div.className = "pagination";
 page.appendChild(div);
 div.appendChild(ul);
 
+// This loop adds two elements to create numbere buttons based on the quotient from the length of the student list being divided by 10 at the bottom of the page. 
 for (let i = 1; i <= pageNumbers; i++){
    const li = document.createElement('li');
    const a = document.createElement ('a');
@@ -72,21 +48,14 @@ for (let i = 1; i <= pageNumbers; i++){
    a.addEventListener('click', (e) => {
       showPage(list, i);
       const eachA = document.querySelectorAll("a");
-
-      
-  if (event.target.tagName === 'a')
+// This snipit highlights the button belonging to the page that is currently active and unhighlights it when the page is no longer being viewed. 
    for (let i=0; i < eachA.length; i++){
      eachA[i].classList.remove("active");
    }
      e.target.className = "active";
-      //Something is wrong here. The buttons are kept highlighted after they've been clicked. 
-   })
-     
+    })
    }
- 
 };
+
+// The appendPageLinks function is called. 
 appendPageLinks(shorterList);
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
